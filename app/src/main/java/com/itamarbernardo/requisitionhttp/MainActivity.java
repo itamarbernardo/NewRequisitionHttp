07d879de4908d3d00b56e7b73770f248e24ca2ec
Itamar Bernardo <itamarbernardo2013@gmail.com>
2016-12-08 20:58:44 -0300

    Concertado action botão

--- app/src/main/java/com/itamarbernardo/requisitionhttp/MainActivity.java
+++ app/src/main/java/com/itamarbernardo/requisitionhttp/MainActivity.java
@@ -16,18 +16,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AcessoRest ac = new AcessoRest();
-        String urlMaps = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=Seattle&destinations=San+Francisco&key=AIzaSyDxSPaFPYw4VHoODgbDcoRWjj68zeUkEu8";
-        String wsLocal = "http://192.168.0.104:8084/MyNewHome/webresources/sensor?sensorId=1";
-        String alunosWS = "http://192.168.0.104:8084/VideoAulaWebServices/webresources/aulaws/usuario/get";
-        mensagem = ac.exemploGet(wsLocal);
-        Log.i("JSON", mensagem);
-        editText = (EditText) findViewById(R.id.editText);
 
     }
 
     public void mostraMsg(View arg0){
+        AcessoRest ac = new AcessoRest();
+        String urlMaps = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=Seattle&destinations=San+Francisco&key=AIzaSyDxSPaFPYw4VHoODgbDcoRWjj68zeUkEu8";
+        String wsLocal = "http://192.168.0.104:8084/MyNewHome/webresources/sensor?sensorId=1";
+        String alunosWS = "http://192.168.0.104:8084/VideoAulaWebServices/webresources/aulaws/usuario/get";
+
+        mensagem = ac.exemploGet(wsLocal);
+        Log.i("JSON", mensagem);
+        editText = (EditText) findViewById(R.id.editText);
         editText.setText(mensagem);
+
     }
 
 
